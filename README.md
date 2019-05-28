6f22cfab0acad5c9608df9d2d6b14457ced2a150
Samuel Ramos <samuelramox@outlook.com>
2019-05-27 21:11:36 -0300

    feat: add files

---
+++ README.md
@@ -0,0 +1,53 @@
+# Windows 10 Setup Script
+
+This is a scripts to setup `Windows 10` using `PowerShell`. You can modify the scripts to fit your own requirements.
+
+## Installation
+
+If you already have `Windows 10`, run these commands:
+
+```
+git clone https://github.com/samuelramox/windows-setup.git && cd windows-setup
+
+./install/setup.ps1
+```
+
+## Steps
+
+- Set a new computer name
+- Remove a few pre-installed `UWP` applications:
+  - Candy Crush Saga
+  - Fitbit FitbitCoach
+  - Microsoft 3D Builder
+  - Microsoft 3D Viewer
+  - Microsoft Bing News
+  - Microsoft One Connect
+  - Microsoft Print 3D
+  - Microsoft Solitaire Collection
+  - Microsoft Windows Feedback
+  - Netflix
+- Install `Chocolatey` and some apps:
+  - [Fluent Terminal](https://github.com/felixse/FluentTerminal)
+  - [Fira Code Font](https://github.com/tonsky/FiraCode)
+  - [Franz](https://meetfranz.com/)
+  - [Git Credential Manager for Windows](https://github.com/Microsoft/Git-Credential-Manager-for-Windows)
+  - [Google Chrome](https://www.google.com/chrome/)
+  - [PeaZip](http://www.peazip.org/)
+  - [qBittorrent](https://www.qbittorrent.org/index.php)
+  - [Visual Studio Code](https://chocolatey.org/packages/vscode)
+  - [Visual Studio Code Insiders](https://code.visualstudio.com/insiders/)\*
+- Enable PUA Protection
+- Raise UAC level
+- Disable Remote Desktop
+- Disable Autoplay
+- Disable Autorun for all drives
+- Disable built-in Adobe Flash in IE and Edge
+- Disable Windows Update P2P delivery optimization (WUDO) completely
+- Install WSL and Ubuntu 18.04
+
+\* _So far, `Visual Studio Code Insiders` are required to use the [Remote - WSL](https://aka.ms/vscode-remote/download/wsl) extension._
+
+## References and more options
+
+- [EdiWang EnvSetup](https://github.com/EdiWang/EnvSetup/)
+- [Win10 Initial Setup Script](https://github.com/Disassembler0/Win10-Initial-Setup-Script)
---
+++ script.ps1
@@ -0,0 +1,156 @@
+# Windows 10 Setup Script
+# Font: https://github.com/samuelramox/windows-setup
+# Run this script in PowerShell
+
+# -----------------------------------------------------------------------------
+# Self elevate administrative permissions  in this script
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+function Check-Command($cmdname) {
+  return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
+# -----------------------------------------------------------------------------
+# Set a new computer name
+$computerName = Read-Host 'Enter New Computer Name'
+Write-Host "Renaming this computer to: " $computerName  -ForegroundColor Yellow
+Rename-Computer -NewName $computerName
+
+# -----------------------------------------------------------------------------
+# Remove a few pre-installed UWP applications
+# To list all appx packages:
+# Get-AppxPackage | Format-Table -Property Name,Version,PackageFullName
+Write-Host "Removing UWP Rubbish..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+$uwpRubbishApps = @(
+  "king.com.CandyCrushSaga",
+  "Microsoft.3DBuilder",
+  "Microsoft.Print3D",
+  "Microsoft.BingNews",
+  "Microsoft.OneConnect",
+  "Microsoft.Microsoft3DViewer",
+  "Microsoft.MicrosoftSolitaireCollection",
+  "Microsoft.WindowsFeedbackHub",
+  "Fitbit.FitbitCoach",
+  "4DF9E0F8.Netflix")
+
+foreach ($uwp in $uwpRubbishApps) {
+  Get-AppxPackage -Name $uwp | Remove-AppxPackage
+}
+
+# -----------------------------------------------------------------------------
+# Install Chocolatey and some apps
+if (Check-Command -cmdname 'choco') {
+  Write-Host "Choco is already installed, skip installation."
+}
+else {
+  Write-Host ""
+  Write-Host "Installing Chocolatey for Windows..." -ForegroundColor Green
+  Write-Host "------------------------------------" -ForegroundColor Green
+  Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Write-Host ""
+Write-Host "Installing Applications..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+
+if (Check-Command -cmdname 'git') {
+  Write-Host "Git is already installed, checking new version..."
+  choco update git -y
+}
+else {
+  Write-Host ""
+  Write-Host "Installing Git for Windows..." -ForegroundColor Green
+  choco install git -y
+}
+
+if (Check-Command -cmdname 'node') {
+  Write-Host "Node.js is already installed, checking new version..."
+  choco update nodejs -y
+}
+else {
+  Write-Host ""
+  Write-Host "Installing Node.js..." -ForegroundColor Green
+  choco install nodejs -y
+}
+
+choco install peazip -y
+choco install googlechrome -y
+choco install vscode -y
+choco install vscode-insiders -y
+choco install fluent-terminal -y
+choco install git-credential-manager-for-windows -y
+choco install firacode-ttf -y
+choco install franz -y
+choco install qbittorrent -y
+
+# -----------------------------------------------------------------------------
+# Install WSL and Ubuntu 18.04
+Write-Host ""
+Write-Host "Installing WSL..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+curl.exe -L -o ubuntu-1804.appx https://aka.ms/wsl-ubuntu-1804
+
+# -----------------------------------------------------------------------------
+# Enable PUA Protection in Windows Defender
+Write-Host "Enabling PUA Protection in Windows Defender" -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Set-MpPreference -PUAProtection 1
+
+# -----------------------------------------------------------------------------
+# Raise UAC level
+Write-Host "Raising UAC level..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name"ConsentPromptBehaviorAdmin" -Type DWord -Value 5
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name"PromptOnSecureDesktop" -Type DWord -Value 1
+
+# -----------------------------------------------------------------------------
+# Disable Remote Desktop
+Write-Host "Disabling Remote Desktop..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Type DWord -Value 1
+Disable-NetFirewallRule -Name "RemoteDesktop*"
+
+# -----------------------------------------------------------------------------
+# Disable Autoplay
+Write-Host "Disabling Autoplay..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 1
+
+# -----------------------------------------------------------------------------
+# Disable Autorun for all drives
+Write-Host "Disabling Autorun for all drives..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
+  New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun" -Type DWord -Value 255
+
+# -----------------------------------------------------------------------------
+# Disable built-in Adobe Flash in IE and Edge
+Write-Host "Disabling built-in Adobe Flash in IE and Edge..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer")) {
+  New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer" -Name "DisableFlashInIE" -Type DWord -Value 1
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Addons")) {
+  New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Addons" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Addons" -Name "FlashPlayerEnabled" -Type DWord -Value 0
+
+# -----------------------------------------------------------------------------
+# Disable Windows Update P2P delivery optimization (WUDO) completely
+Write-Host "Disabling Windows Update P2P optimization (WUDO)..." -ForegroundColor Green
+Write-Host "------------------------------------" -ForegroundColor Green
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization")) {
+  New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" -Name "DODownloadMode" -Type DWord -Value 100
+
+# -----------------------------------------------------------------------------
+# Restart Windows
+Write-Host "------------------------------------" -ForegroundColor Green
+Read-Host -Prompt "Setup is done, restart is needed, press [ENTER] to restart computer."
+Restart-Computer
