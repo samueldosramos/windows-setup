b87f01e43c86df9cb9d773624473a6a6fb17d8a2
Samuel Ramos <samuelramox@outlook.com>
2019-08-11 23:24:05 -0300

    feat: remove franz

--- script.ps1
+++ script.ps1
@@ -82,7 +82,6 @@ choco install peazip -y
 choco install googlechrome -y
 choco install vscode -y
 choco install firacode-ttf -y
-choco install franz -y
 choco install qbittorrent -y
 
 # -----------------------------------------------------------------------------
