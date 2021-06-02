ecbd8bc377a7f887c1dedd1bd8a66fb234a61695
Samuel Ramos <samuelramox@outlook.com>
2021-06-02 00:19:00 -0300

    feat: add script to use in work setup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Windows 10 Setup Script
 
-This is a scripts to setup `Windows 10` using `PowerShell`.  
+This is a scripts to setup `Windows 10` using `PowerShell`.
 **Note:** _You can modify the scripts to fit your own requirements._
 
 ## Installation
@@ -8,68 +8,5 @@ This is a scripts to setup `Windows 10` using `PowerShell`.
 If you already have `Windows 10`, run these commands in `PowerShell`:
 
 ```
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/samuelramox/windows-setup/master/script.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/samuelramox/windows-setup/work/script.ps1'))
 ```
-
-## Steps
-
-- Set a new computer name
-- Remove a few pre-installed `UWP` applications:
-  - Candy Crush
-  - Fitbit FitbitCoach
-  - Microsoft 3D Builder
-  - Microsoft 3D Viewer
-  - Microsoft Bing News
-  - Microsoft Mixed Reality Portal
-  - Microsoft One Connect
-  - Microsoft Print 3D
-  - Microsoft Solitaire Collection
-  - Microsoft Tips
-  - Microsoft Windows Feedback
-  - Microsoft Xbox
-  - Netflix
-  - Windows Mixed Reality
-- Install `Chocolatey` and some apps:
-  - [Fira Code Font](https://github.com/tonsky/FiraCode)
-  - [Git](https://gitforwindows.org/) (with --params "/NoShellIntegration /NoAutoCrlf /SChannel /WindowsTerminal")
-  - [Google Chrome](https://www.google.com/chrome/)
-  - [Node.js LTS](https://nodejs.org/)
-  - [PeaZip](http://www.peazip.org/)
-  - [qBittorrent](https://www.qbittorrent.org/index.php)
-  - [Visual Studio Code](https://chocolatey.org/packages/vscode)
-- Enable PUA Protection
-- Disable Autoplay
-- Disable Autorun for all drives
-- Disable built-in Adobe Flash in IE and Edge
-- Disable Windows Update P2P delivery optimization (WUDO) completely
-- Install `oh-my-posh` and change `Set-ExecutionPolicy` to `Unrestricted`
-- Install WSL and enable Hyper-V
-
-## Manual setup after installation (optional)
-
-### Terminal options:
-
-- [Windows Terminal](https://www.microsoft.com/en-us/p/windows-terminal-preview/9n0dx20hk701?activetab=pivot%3Aoverviewtab)
-- [Hyper](https://hyper.is)
-- [Terminus](https://eugeny.github.io/terminus/)
-- [Fluent Terminal](https://github.com/felixse/FluentTerminal)
-
-### Enable clipboard history
-
-Open the `Settings` app and go to the `System group of settings`. Select the `Clipboard` tab, and turn on `Clipboard History`.
-
-### Install QuickLook
-
-Allows user peek into a file content in lightning speed by just pressing the Space key.
-Link in Microsoft Store: [QuickLook](https://www.microsoft.com/pt-br/p/quicklook/9nv4bs3l1h4s?ocid=badge&rtc=1&activetab=pivot:overviewtab)
-
-### Block non-Store apps
-
-Settings -> Apps -> Apps & features -> The Microsoft Store only (recommended).
-**Note:** _I only do this when I've installed everything I need._
-
-## References and more options
-
-- [EdiWang EnvSetup](https://github.com/EdiWang/EnvSetup/)
-- [Win10 Initial Setup Script](https://github.com/Disassembler0/Win10-Initial-Setup-Script)
-- [oh-my-posh](https://github.com/JanDeDobbeleer/oh-my-posh)
--- script.ps1
+++ script.ps1
@@ -1,5 +1,4 @@
 # Windows 10 Setup Script
-# Font: https://github.com/samuelramox/windows-setup
 # Run this script in PowerShell
 
 # -----------------------------------------------------------------------------
@@ -11,39 +10,7 @@ function Check-Command($cmdname) {
 }
 
 # -----------------------------------------------------------------------------
-# Set a new computer name
-$computerName = Read-Host 'Enter New Computer Name'
-Write-Host "Renaming this computer to: " $computerName  -ForegroundColor Yellow
-Rename-Computer -NewName $computerName
-
-# -----------------------------------------------------------------------------
-# Remove a few pre-installed UWP applications
-# To list all appx packages:
-# Get-AppxPackage | Format-Table -Property Name,Version,PackageFullName
-Write-Host "Removing UWP Rubbish..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-$uwpRubbishApps = @(
-  "king.com.CandyCrushFriends",
-  "Microsoft.3DBuilder",
-  "Microsoft.Print3D",
-  "Microsoft.BingNews",
-  "Microsoft.OneConnect",
-  "Microsoft.Microsoft3DViewer",
-  "HolographicFirstRun",
-  "Microsoft.MixedReality.Portal"
-  "Microsoft.MicrosoftSolitaireCollection",
-  "Microsoft.Getstarted",
-  "Microsoft.WindowsFeedbackHub",
-  "Microsoft.XboxApp",
-  "Fitbit.FitbitCoach",
-  "4DF9E0F8.Netflix")
-
-foreach ($uwp in $uwpRubbishApps) {
-  Get-AppxPackage -Name $uwp | Remove-AppxPackage
-}
-
-# -----------------------------------------------------------------------------
-# Install Chocolatey and some apps
+# Install Chocolatey and apps
 if (Check-Command -cmdname 'choco') {
   Write-Host "Choco is already installed, skip installation."
 }
@@ -55,7 +22,7 @@ else {
 }
 
 Write-Host ""
-Write-Host "Installing Applications..." -ForegroundColor Green
+Write-Host "Installing Windows apps..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 
 if (Check-Command -cmdname 'git') {
@@ -68,64 +35,19 @@ else {
   choco install git --params "/NoShellIntegration /NoAutoCrlf /SChannel /WindowsTerminal" -y
 }
 
-if (Check-Command -cmdname 'node') {
-  Write-Host "Node.js is already installed, checking new version..."
-  choco update nodejs -y
-}
-else {
-  Write-Host ""
-  Write-Host "Installing Node.js..." -ForegroundColor Green
-  choco install nodejs-lts -y
-}
+APPS=(
+  beekeeper-studio
+  googlechrome
+  insomnia
+  slack
+  vscode
+  zoom
+)
 
-choco install peazip -y
-choco install googlechrome -y
-choco install vscode -y
-choco install firacode-ttf -y
-choco install qbittorrent -y
-
-# -----------------------------------------------------------------------------
-# Enable PUA Protection in Windows Defender
-Write-Host "Enabling PUA Protection in Windows Defender" -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Set-MpPreference -PUAProtection 1
-
-# -----------------------------------------------------------------------------
-# Disable Autoplay
-Write-Host "Disabling Autoplay..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Type DWord -Value 1
-
-# -----------------------------------------------------------------------------
-# Disable Autorun for all drives
-Write-Host "Disabling Autorun for all drives..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
-  New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" | Out-Null
-}
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun" -Type DWord -Value 255
-
-# -----------------------------------------------------------------------------
-# Disable built-in Adobe Flash in IE and Edge
-Write-Host "Disabling built-in Adobe Flash in IE and Edge..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer")) {
-  New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer" -Force | Out-Null
-}
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer" -Name "DisableFlashInIE" -Type DWord -Value 1
-If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Addons")) {
-  New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Addons" -Force | Out-Null
-}
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Addons" -Name "FlashPlayerEnabled" -Type DWord -Value 0
-
-# -----------------------------------------------------------------------------
-# Disable Windows Update P2P delivery optimization (WUDO) completely
-Write-Host "Disabling Windows Update P2P optimization (WUDO)..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization")) {
-  New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" | Out-Null
-}
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" -Name "DODownloadMode" -Type DWord -Value 100
+for app in ${APPS[@]}
+do
+  choco install $app -y
+done
 
 # -----------------------------------------------------------------------------
 # Install oh-my-posh and change Set-ExecutionPolicy to "Unrestricted"
@@ -134,12 +56,15 @@ Install-Module oh-my-posh -Scope CurrentUser
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 
 # -----------------------------------------------------------------------------
-# Install WSL
+# Install WSL and Ubuntu
 Write-Host ""
 Write-Host "Installing WSL..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+wsl --set-default-version 2
+curl.exe -L -o ubuntu.appx https://aka.ms/wsl-ubuntu
+Add-AppxPackage .\ubuntu.appx
 
 # -----------------------------------------------------------------------------
 # Restart Windows
